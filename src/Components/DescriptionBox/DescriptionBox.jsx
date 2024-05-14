d8f7c13636c1bc35adb91a870c79162a74c85711
Michael Chukwu <imchukwu@gmail.com>
2024-05-14 18:29:31 +0100

    done with laptop screen view

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Ultimately, any of these cloud platforms offer strong career prospects
+          in Europe. The best choice depends on your specific goals, interests,
+          and the job market in your target locations. You can't go wrong with
+          gaining expertise in any of these leading platforms, and your
+          adaptability and willingness to learn new technologies will be
+          valuable assets in the ever-evolving cloud computing landscape.
+          gaining expertise in any of these leading platforms, and your
+          adaptability and willingness to learn new technologies will be
+          valuable assets in the ever-evolving cloud computing landscape.
+        </p>
+        <p>
+          gaining expertise in any of these leading platforms, and your
+          adaptability and willingness to learn new technologies will be
+          valuable assets in the ever-evolving cloud computing landscape.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default DescriptionBox;
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import Breadcrum from "../Components/Breadcrums/Breadcrum";
-import { ShopContext } from "../Context/ShopContext";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
@@ -11,7 +13,9 @@ const Product = () => {
   return (
     <div>
       <Breadcrum product={product} />
-      <ProductDisplay product={product}/>
+      <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts />
     </div>
   );
 };
